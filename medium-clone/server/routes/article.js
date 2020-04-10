83f3317b85c29b29640b46a7175264e23d0d9856
Keshav Kansal <kansalkeshav@gmail.com>
2020-04-10 08:42:00 +0530

    Fixed Bug in Get All Articles

--- medium-clone/server/routes/article.js
+++ medium-clone/server/routes/article.js
@@ -7,7 +7,8 @@ const articleController = require('../controllers/article');
 router.post('/newArticle',checkAuth,articleController.create_new_article);
 router.post('/like',checkAuth,articleController.clap_article);
 router.post('/comment',checkAuth,articleController.comment_article);
-router.get('/:id',checkAuth,articleController.get_article);
 router.get('/getAll',checkAuth,articleController.get_all_article);
+router.get('/:id',checkAuth,articleController.get_article);
+
 
 module.exports = router;
